ef850aa7729b39877c712700e53aff3f8735993a
Mahesh Kolluri <maheshkolluri8179@gmail.com>
2023-07-05 22:23:12 +0530

    Node JS Basics

---
+++ app.js
@@ -0,0 +1,7 @@
+var a = 7
+var b = 8
+
+var add = a+b
+console.log("sum is:"+add)
+
+console.log("Hello world")
---
+++ create_server.js
@@ -0,0 +1,8 @@
+var http = require('http')
+console.log("working")
+
+http.createServer(function(req, res){ 
+    res. 
+    res.write("Welcome !!!")
+    res.end()
+}).listen(8080)
